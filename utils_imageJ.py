b8e3441ae2eb7f2f7a6a047f0e852cadadcf2c53
王泽昊 <wangze@mail.ustc.edu.cn>
2021-12-08 15:04:24 +0800

    Update utils_imageJ.py

--- utils_imageJ.py
+++ utils_imageJ.py
@@ -181,36 +181,3 @@ def save_tiff_imagej_compatible(file, img, axes, **imsave_kwargs):
 
     imsave_kwargs['imagej'] = True
     imsave(file, img, **imsave_kwargs)
-
-# def save_tiff_imagej_compatible(file, img, axes, **imsave_kwargs):
-#     """Save image in ImageJ-compatible TIFF format.
-
-#     Parameters
-#     ----------
-#     file : str
-#         File name
-#     img : numpy.ndarray
-#         Image
-#     axes: str
-#         Axes of ``img``
-#     imsave_kwargs : dict, optional
-#         Keyword arguments for :func:`tifffile.imsave`
-
-#     """
-#     axes = axes_check_and_normalize(axes,img.ndim)
-
-#     # convert to imagej-compatible data type
-#     t = img.dtype
-#     if   'float' in t.name: t_new = np.float32
-#     elif 'uint'  in t.name: t_new = np.uint16 if t.itemsize >= 2 else np.uint8
-#     elif 'int'   in t.name: t_new = np.int16
-#     else:                   t_new = t
-#     img = img.astype(t_new, copy=False)
-#     if t != t_new:
-#         warnings.warn("Converting data type from '%s' to ImageJ-compatible '%s'." % (t, np.dtype(t_new)))
-
-#     # move axes to correct positions for imagej
-#     img = move_image_axes(img, axes, 'STZCYX', True)
-
-#     imsave_kwargs['imagej'] = True
-#     imsave(file, img, **imsave_kwargs)
